f2a63b8ea74efc5b03db7e35729bfa1dc6aa8d43
Emmanuel E. Ulloa Dominguez <46717658+HermitCrow@users.noreply.github.com>
2019-09-04 10:26:57 -0400

    prueba

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-
-?>
+<!DOCTEPY HTML>
+<html Lang="ES">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Imprimir PHP</title>
+  </head>
+  <body>
+  <h1>Hola Mundo desde HTML</h1>
+    <? hola mundo desde PHP?>
+  </body>
+</html>  
