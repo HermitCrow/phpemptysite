7eebe1a43b88904886d2fea072cc08d5890d4785
Emmanuel E. Ulloa Dominguez <46717658+HermitCrow@users.noreply.github.com>
2019-09-04 10:28:58 -0400

    prueba2
    actualizacion

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php
-
-?>
+<!DOCTYPE HTML>
+<html LANG="ES">
+    <head>
+        <meta charset="utf-8"/>
+        <TITLE>Imprimis PHP</TITLE>
+    </head>
+    <body>
+        <h1>Master en PHP - Emmanuel Ulloa</h1>
+        
+        <?php
+            //Imprime el listado de juegos
+             echo "<h3>Listado de video juegos:</h3>";
+             echo "<ul>"
+                  ."<li><strong>GTA</strong> :"
+                     ."<ul>"
+                     ."<li>Vice City</li>"
+                     ."<li>San Andreas</li>"
+                     ."<li>5</li>"
+                     ."</ul>"
+                  ."</li>"   
+                  ."<li><strong>ARK</strong></li>"
+                  ."<li><strong>PKG</strong></li>"
+                  ."<li><strong>Zoid</strong></li>"   
+                  ."</ul>";
+             echo '<p>parafo '.'-'.' carlito';
+        ?>
+    </body>
+</html>
